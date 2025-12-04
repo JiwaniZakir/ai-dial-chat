7c54e9bbfb402a72db1e382bb5371f712fee70b8
Ilya Bondar <ilya_bondar@epam.com>
2025-12-04 15:42:23 +0100

    chore: bump NX from 21.6.5 to 22.1.3

--- apps/chat/next.config.js
+++ apps/chat/next.config.js
@@ -34,7 +34,7 @@ class BasePathResolver {
    * @param {string} str
    */
   startsWith(str) {
-    return this.valueOf().startsWith(str)
+    return this.valueOf().startsWith(str);
   }
 
   /**
@@ -49,7 +49,7 @@ class BasePathResolver {
    * @param {string} str
    */
   endsWith(str) {
-    return this.valueOf().endsWith(str)
+    return this.valueOf().endsWith(str);
   }
 
   toJSON() {
@@ -73,7 +73,8 @@ const nextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
   // @ts-ignore
-  basePath: process.env.NODE_ENV !== 'development' ? new BasePathResolver() : '',
+  basePath:
+    process.env.NODE_ENV !== 'development' ? new BasePathResolver() : '',
 
   async redirects() {
     return [
@@ -95,17 +96,13 @@ const nextConfig = {
       // Support old two route app editor links
       {
         source: '/apps-editor/:slug/settings',
-        has: [
-          { type: 'query', key: 'id', value: '(?<id>.*)' }
-        ],
+        has: [{ type: 'query', key: 'id', value: '(?<id>.*)' }],
         destination: '/apps-editor?step=General&schema=:slug&id=:id',
         permanent: false,
       },
       {
         source: '/apps-editor/:slug',
-        has: [
-          { type: 'query', key: 'id', value: '(?<id>.*)' }
-        ],
+        has: [{ type: 'query', key: 'id', value: '(?<id>.*)' }],
         destination: '/apps-editor?step=General&schema=:slug&id=:id',
         permanent: false,
       },
@@ -133,8 +130,9 @@ const nextConfig = {
 
     //SVGR config
     // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((/** @type {{ test: { test: (arg0: string) => any; }; }} */ rule) =>
-      rule.test?.test?.('.svg'),
+    const fileLoaderRule = config.module.rules.find(
+      (/** @type {{ test: { test: (arg0: string) => any; }; }} */ rule) =>
+        rule.test?.test?.('.svg'),
     );
 
     config.module.rules.push(
@@ -194,7 +192,6 @@ const nextConfig = {
       },
     ];
   },
-
 };
 
 const plugins = [
